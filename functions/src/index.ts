2875f499adf1b050a1f6ffc638271710d6f3497f
Greg Santos <gregsantos@gmail.com>
2020-09-28 17:16:44 -0700

    Update fb functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions'
-// import * as admin from 'firebase-admin'
-const admin = require('firebase-admin')
+import * as admin from 'firebase-admin'
+// const functions = require('firebase-functions')
+// const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
