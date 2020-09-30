f2d36f98272a5e1d84718e3b55ada15834a26c89
Greg Santos <gregsantos@gmail.com>
2020-09-30 15:57:13 -0700

    Add merchant.js, wallet state and chair ledger

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,46 @@
 import React, { useState } from 'react'
 import { Flex, Box, Text, Button } from '@chakra-ui/core'
 import Terminal from 'react-console-emulator'
+import { Map } from 'immutable'
+import { add, sum, buy, inTheBlack, effects } from 'merchant.js'
 import Logo from '../components/Logo'
 import Newsbar from '../components/Newsbar'
 import { useInterval } from '../hooks/useInterval'
 import Counter from '../components/Counter'
 // import onLoad from '../utils/'
 
-type IndexProps = { name: string }
-
-const diff =
+const initialTime =
   new Date().setFullYear(new Date().getFullYear() + 62) - Math.floor(Date.now())
 
-const initialTime = diff
 const interval = 100
 
+const initialState = {
+  clicks: { amount: 0 },
+  auto: { cost: 10, amount: 0 },
+}
+
+// Currencies
+const DOLLARS = 'DOLLARS'
+
+// Items
+const pouch = {
+  chair: {
+    type: 'Chair',
+    cost: () => {
+      return Map({ [DOLLARS]: -10 })
+    },
+    effect: () => {
+      return Map({ [DOLLARS]: 1 })
+    },
+  },
+}
+
+type IndexProps = { name: string }
+
 export const Index = ({ name = 'anonymous' }: IndexProps) => {
-  //   const [response, setResponse] = useState<null | { price: number }>(null);
+  const [wallet, setWallet] = useState(Map())
+  const [ledger, setLedger] = useState(Map())
+
   const [state, setState] = useState({
     netWorth: 0,
     count: initialTime,
@@ -24,8 +48,35 @@ export const Index = ({ name = 'anonymous' }: IndexProps) => {
   })
 
   useInterval(() => {
-    setState({ ...state, count: state.count - (3600 / 60) * 1000 })
+    update()
   }, interval)
+
+  const update = () => {
+    setWallet(sum(wallet, ledger))
+    setState({
+      ...state,
+      count: state.count - (3600 / 60) * 1000,
+    })
+  }
+
+  const work = () => {
+    const newWallet = sum(wallet, Map({ [DOLLARS]: 1 }))
+    setWallet(newWallet)
+  }
+
+  const buyChair = () => {
+    const walletWithCostsApplied = buy(pouch.chair, wallet)
+    if (!inTheBlack(walletWithCostsApplied)) {
+      alert("You can't afford this upgrade")
+      return
+    }
+
+    const newWallet = add(pouch.chair, walletWithCostsApplied)
+    const newLedger = effects(Object.values(pouch), newWallet)
+
+    setWallet(newWallet)
+    setLedger(newLedger)
+  }
 
   const click = () => {
     setState({
@@ -39,31 +90,59 @@ export const Index = ({ name = 'anonymous' }: IndexProps) => {
     <Box
       display={{ base: 'flex', md: 'grid' }}
       flexDirection='column'
-      gridTemplateColumns='minmax(245px, 1fr) 2fr minmax(245px, 1fr)'
-      gridTemplateRows='auto 1fr auto'
+      gridTemplateColumns='minmax(100px, 1fr) 640px minmax(100px, 1fr)'
+      gridTemplateRows='75px 1fr auto'
       minHeight='100vh'
     >
       <header>
-        <Flex p={3} direction='column' align='center'>
-          <Logo width='50' height='25' />
-          <h1>DOOMBERG</h1>
-          <h3>{name}</h3>
-        </Flex>
+        <Logo width='25' height='25' />
+        <h3>DOOMBERG</h3>
       </header>
       <Box p={3} border='1px solid' borderColor='green.300'>
         <Flex
           direction='column'
           justify='center'
-          padding='20px'
+          padding={3}
           color='green.300'
           border='1px solid'
           borderColor='green.300'
-          zIndex={100}
         >
           <Box color='green.300'>Net Worth</Box>
-          <Box>$ {state.netWorth}</Box>
+          <Box mb={2}>$ {wallet.get(DOLLARS) || 0}</Box>
           <Box color='green.300'>Time till Death</Box>
           <Counter timeLeft={state.count} />
+          <Button
+            my={2}
+            onClick={work}
+            variantColor='green'
+            variant='outline'
+            zIndex={100}
+            _hover={{ bg: 'green.200' }}
+          >
+            Work
+          </Button>
+        </Flex>
+        <Flex
+          direction='column'
+          justify='center'
+          mt={3}
+          padding={3}
+          color='green.300'
+          border='1px solid'
+          borderColor='green.300'
+        >
+          <Box mb={2}>Dollars per second: {ledger.get(DOLLARS) || 0 / 5}</Box>
+          <Button
+            mb={3}
+            onClick={buyChair}
+            variantColor='green'
+            variant='outline'
+            zIndex={100}
+            _hover={{ bg: 'green.200' }}
+          >
+            Buy a Chair
+          </Button>
+          <h1> Chairs: {wallet.get(pouch.chair.type) || 0} </h1>
         </Flex>
       </Box>
       <Flex
@@ -71,19 +150,21 @@ export const Index = ({ name = 'anonymous' }: IndexProps) => {
         p={3}
         justify='center'
         align='center'
+        direction='column'
         border='1px solid'
         borderColor='green.300'
       >
         <Terminal
           autoFocus
+          ignoreCommandCase
+          promptLabel={'$'}
           welcomeMessage={'Welcome to the Doomberg terminal!'}
-          promptLabel={'me@DOOMBERG:~$'}
           style={{
-            height: '100%',
+            height: '75%',
             width: '100%',
             alignSelf: 'flex-start',
-            minHeight: '50px',
           }}
+          contentStyle={{ overflow: 'scroll', maxHeight: '192px' }}
           commandCallback={(result) => console.log(result)}
           commands={{
             echo: {
@@ -95,6 +176,16 @@ export const Index = ({ name = 'anonymous' }: IndexProps) => {
             },
           }}
         />
+        <Flex
+          flex={1}
+          width='100%'
+          mt={3}
+          p={3}
+          justify='center'
+          align='center'
+          border='1px solid'
+          borderColor='green.300'
+        ></Flex>
       </Flex>
       <Box p={3} border='1px solid' borderColor='green.300'>
         <Flex
@@ -105,25 +196,7 @@ export const Index = ({ name = 'anonymous' }: IndexProps) => {
           border='1px solid'
           borderColor='green.300'
         >
-          <Button
-            mb={3}
-            onClick={click}
-            variantColor='green'
-            variant='outline'
-            zIndex={100}
-          >
-            Work
-          </Button>
           <Flex id='slider' align='center' direction={['row', 'row', 'column']}>
-            <Flex align='center' m={2} zIndex={100}>
-              <Text fontSize='sm' mr={2}>
-                Leverage
-              </Text>
-              <label className='switch'>
-                <input type='checkbox' />
-                <span className='slider'></span>
-              </label>
-            </Flex>
             <Flex align='center' m={2} zIndex={100}>
               <Text fontSize='sm' mr={2}>
                 Leverage
